ae5b7afc7ce1d51349bbdae413cc14381d3deada
JonathanMendler <131711076+JonathanMendler@users.noreply.github.com>
2023-07-05 13:38:34 -0500

    Adds Modal.css & .jsx, modified Content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { StudentsShow } from "./StudentsShow";
+import { Modal } from "./Modal";
+
 export function Content() {
+  const [students, setStudents] = useState([]);
+  const [isStudentsShowVisible, setIsStudentsShowVisible] = useState(false);
+  const [currentStudent, setCurrentStudent] = useState({});
+
+  const handleShowStudent = (student) => {
+    console.log("handleShowStudent", student);
+    setIsStudentsShowVisible(true);
+    setCurrentStudent(student);
+  };
+
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsStudentsShowVisible(false);
+  };
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <Modal show={isStudentsShowVisible} onClose={handleClose}>
+        + <h1>Test</h1>+{" "}
+      </Modal>
     </div>
   );
 }
